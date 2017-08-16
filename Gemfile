380b55606107259314456746bafa01a3730e03e7
Radhika Kancharla <radhika.kancharla@mckesson.com>
2017-08-16 18:38:48 +0000

    changes

--- Gemfile
+++ Gemfile
@@ -29,6 +29,12 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 group :development, :test do
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'faker'
+  gem 'annotate'
+  gem 'railroady'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,17 @@
+class CitiesController < ApplicationController
+    def index
+        @cities= City.all
+    end
+    def new
+        @city= City.new
+    end
+    def create
+        @city = City.new(params[:city].permit(:name, :country, :photo))
+        if @city.save
+            redirect_to cities_path
+        else
+            render 'new'
+        end
+    end
+
+end
